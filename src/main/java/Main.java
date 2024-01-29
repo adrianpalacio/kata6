95c3d9f9473d124662f38e1e153ef4328628379a
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:50:31 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,7 @@
 package org.example;
 
-public class Name {
-    final static String alphabet = "abcdefghijklmnopqrstuvwxyz";
-    private String name;
-    private int numericValue;
-
-    public String getName() {
-        return name;
+public class Main {
+    public static void main(String[] args) {
+        Tester.test();
     }
-    public int getNumericValue() {
-        return numericValue;
-    }
-    private Name(String input) {
-        this.name = input;
-        this.numericValue = calculateNumericValue(input);
-    }
-    public static Name makeName(String input) {
-        if (isValidName(input)) return new Name(input); else return new Name("");
-    }
-    public static boolean isValidName(String input) {
-        return input.matches("[a-zA-Z]+");
-    }
-
-    private int calculateNumericValue(String iname) {
-        if (iname.isEmpty()) return -1; else {
-            int returner = 0;
-            String buffer = iname.toLowerCase();
-            for(char i : buffer.toCharArray()) {
-                returner += alphabet.indexOf(i) + 1;
-            }
-            int buffer2;
-            while ((buffer2 = returner) > 9) {
-                returner = 0;
-                for (char i : String.valueOf(buffer2).toCharArray()) {
-                    returner += Integer.parseInt(String.valueOf(i));
-                }
-            }
-            return returner;
-
-        }
-    }
-
 }
